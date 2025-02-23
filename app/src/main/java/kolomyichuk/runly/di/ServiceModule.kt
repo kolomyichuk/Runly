516a1fd13880705975ed75ca447b5a757376a049
Volodymyr Kolomyichuk <wowakolom@gmail.com>
2025-02-23 20:08:52 +0200

    Create Service Module with provide notification builder and fuded location

---
+++ app/src/main/java/kolomyichuk/runly/di/ServiceModule.kt
@@ -0,0 +1,59 @@
+package kolomyichuk.runly.di
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+import kolomyichuk.runly.MainActivity
+import kolomyichuk.runly.R
+import kolomyichuk.runly.utils.Constants
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun providesFusedLocationProvider(@ApplicationContext context: Context) =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    @ServiceScoped
+    @Provides
+    fun provideTrackingNotificationBuilder(
+        @ApplicationContext context: Context,
+        pendingIntent: PendingIntent
+    ) = NotificationCompat.Builder(context, Constants.TRACKING_CHANNEL_ID)
+        .setAutoCancel(false)
+        .setOngoing(true)
+        .setOnlyAlertOnce(true)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle(context.getString(R.string.app_name))
+        .setContentText("00:00:00")
+        .setContentIntent(pendingIntent)
+
+
+    @ServiceScoped
+    @Provides
+    fun provideActivityPendingIntent(@ApplicationContext context: Context) =
+        PendingIntent.getActivity(
+            context, 0, Intent(context, MainActivity::class.java).apply {
+                action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+            },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+
+    @ServiceScoped
+    @Provides
+    fun provideNotificationManager(@ApplicationContext context: Context): NotificationManager {
+        return context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+}
