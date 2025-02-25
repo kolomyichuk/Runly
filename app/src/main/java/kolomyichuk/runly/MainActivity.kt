4dd0b04b0d512a5e6f3bcb793238add3ffd3ca35
Volodymyr Kolomyichuk <wowakolom@gmail.com>
2025-02-25 10:58:05 +0200

    Add Settings Screen prototype

--- app/src/main/java/kolomyichuk/runly/MainActivity.kt
+++ app/src/main/java/kolomyichuk/runly/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import kolomyichuk.runly.ui.screens.RunScreen
+import kolomyichuk.runly.ui.screens.SettingsScreen
 import kolomyichuk.runly.ui.theme.RunlyTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RunlyTheme {
-                RunScreen()
+                SettingsScreen()
             }
         }
     }
--- app/src/main/java/kolomyichuk/runly/ui/screens/SettingsScreen.kt
+++ app/src/main/java/kolomyichuk/runly/ui/screens/SettingsScreen.kt
@@ -1,8 +1,42 @@
 package kolomyichuk.runly.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun SettingsScreen(){
-
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Theme",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+            )
+            Switch(
+                checked = true,
+                onCheckedChange = {}
+            )
+        }
+    }
 }
