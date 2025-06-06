1b161c764b29e6d3a5c72047c195904e16489997
Volodymyr Kolomyichuk <wowakolom@gmail.com>
2025-06-06 10:14:13 +0300

    Migration room from 1 to 2 version

---
+++ app/src/main/java/kolomyichuk/runly/data/local/room/Migrations.kt
@@ -0,0 +1,39 @@
+package kolomyichuk.runly.data.local.room
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            """
+            CREATE TABLE IF NOT EXIST `runs_2` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `timestamp` INTEGER NOT NULL,
+            `durationInMillis` INTEGER NOT NULL,
+            `distanceInMeters` REAL NOT NULL,
+            `routePoints` TEXT NOT NULL
+            )
+        """.trimIndent()
+        )
+
+        db.execSQL(
+            """
+            INSERT INTO runs_2 (id, timestamp, durationInMillis, distanceInMeters, routePoints)
+            SELECT id, timestamp, durationInMillis, distanceInMeters, routePoints FROM runs
+        """.trimIndent()
+        )
+
+        db.execSQL(
+            """
+            DROP TABLE runs
+        """.trimIndent()
+        )
+
+        db.execSQL(
+            """
+            ALTER TABLE runs_2 RENAME TO runs
+        """.trimIndent()
+        )
+    }
+}
